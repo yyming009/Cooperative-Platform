f9ada18074158d991697de609f8a60ee4fcc16b9
yyming009 <yyming009@gmail.com>
2018-02-03 23:35:41 -0800

    week1 finished

---
+++ week1/oj-client/src/app/mock-problems.ts
@@ -0,0 +1,28 @@
+import { Problem } from "./models/problem.model";
+
+export const PROBLEMS: Problem[] = [
+  {
+    id: 1,
+    name: "Two sum",
+    desc: 'Given  as  anoisnf a anfoiaf anoafroafa  aowfoaf  afaf  af oanoa   na aa foa of oa foafwnfaowfwo',
+    difficulty: "easy"
+  },
+  {
+    id: 2,
+    name: "Three sum",
+    desc: 'Given  as  anoisnf a anfoiaf anoafroafa  aowfoaf  afaf  af oanoa   na aa foa of oa foafwnfaowfwo',
+    difficulty: "easy"
+  },
+  {
+    id: 3,
+    name: "Four sum",
+    desc: 'Given  as  anoisnf a anfoiaf anoafroafa  aowfoaf  afaf  af oanoa   na aa foa of oa foafwnfaowfwo',
+    difficulty: "hard"
+  },
+  {
+    id: 4,
+    name: "forg jump",
+    desc: 'Given  as  anoisnf a anfoiaf anoafroafa  aowfoaf  afaf  af oanoa   na aa foa of oa foafwnfaowfwo',
+    difficulty: "medium"
+  },
+];
--- week2/oj-client/src/app/app.module.ts
+++ week2/oj-client/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { routing } from "./app.routes";
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProblemListComponent,
-    ProblemDetailComponent
+    ProblemDetailComponent,
+    NewProblemComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
